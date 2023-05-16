2d6e87c65dfc125c7e7c3cdbef5ea4a66a12068b
robert <robert.damian@whitebox.tech>
2023-05-16 21:39:57 +0300

    added sync script and docker-compose-stack files

---
+++ sync.sh
@@ -0,0 +1,7 @@
+(cd ../auth-service && git pull)
+(cd ../database && git pull)
+(cd ../front-end && git pull)
+(cd ../fs-service && git pull)
+(cd ../gateway && git pull)
+(cd ../order-service && git pull)
+(cd ../product-service && git pull)
